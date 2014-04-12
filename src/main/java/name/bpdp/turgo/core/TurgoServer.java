00b4e38b015a8d0e0a89bf564554190e52f5d2f6
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2014-04-12 17:52:38 +0700

    Move executable part to turgo-examples, that way this repo is only for Turgo API etc

--- src/main/java/name/bpdp/turgo/core/TurgoServer.java
+++ src/main/java/name/bpdp/turgo/core/TurgoServer.java
@@ -52,23 +52,4 @@ public class TurgoServer {
 
     }
 
-
-    public static void main(String[] args) throws Exception {
-
-        int port;
-
-        if (args.length > 0) {
-
-            port = Integer.parseInt(args[0]);
-
-        } else {
-
-            port = 8080;
-
-        }
-
-        new TurgoServer(port).run();
-
-    }
-
 }
