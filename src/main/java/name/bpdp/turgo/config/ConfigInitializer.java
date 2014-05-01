d661c7b45cbdbfe5da289dead2dce845dbed896f
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2014-05-01 23:52:53 +0700

    conf/turgo.json for configuration file. add a method in ConfigInitialzer

--- src/main/java/name/bpdp/turgo/config/ConfigInitializer.java
+++ src/main/java/name/bpdp/turgo/config/ConfigInitializer.java
@@ -1,6 +1,6 @@
 package name.bpdp.turgo.config;
 
-import java.io.FileReader;
+import java.io.File;
 
 import javax.json.Json;
 import javax.json.JsonReader;
@@ -11,6 +11,18 @@ import javax.json.JsonStructure;
  * Created by bpdp on 4/14/14.
  */
 public class ConfigInitializer {
+
+	public boolean isConfigExists() {
+
+		File turgoConf = new File("conf/turgo.json");
+ 
+	  if(turgoConf.exists()){
+			return true;
+	  } else {
+			return false;
+	  }	
+
+	}
 
 
 }
